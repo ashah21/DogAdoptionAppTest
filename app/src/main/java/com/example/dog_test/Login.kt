e740a3a430b5819c3ff129116331e4c31b244def
Anya <anya.shah21@gmail.com>
2023-02-20 00:50:30 -0500

    Any inital commits

---
+++ app/src/main/java/com/example/dog_test/Login.kt
@@ -0,0 +1,11 @@
+package com.example.dog_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/dog_test/Register.kt
@@ -0,0 +1,14 @@
+package com.example.dog_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register : AppCompatActivity() {
+
+//    TextInputEditText editTextEmail, editTextPassword;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
